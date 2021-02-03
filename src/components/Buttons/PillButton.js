a49c1b0a7966afa9adb6a5115e83662a5ac5d46a
tariku <tarikutadese24@gmail.com>
2021-02-03 10:26:44 -0500

    transition to react initialized

---
+++ src/components/Buttons/PillButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+const PillButton = ( { text, backgroundColor, clickHandler }) => {
+  return (
+    <button className= {`pill-button pill-button--${backgroundColor}`} onClick={()=> clickHandler(text)}>
+      { text }
+    </button>
+  )
+}
+
+export default PillButton

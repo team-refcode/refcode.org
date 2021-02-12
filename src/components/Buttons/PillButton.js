47f27ee30ff3f4bb6de6424f5b446eca9c02ebb6
tariku <tarikutadese24@gmail.com>
2021-02-11 23:45:33 -0500

    react style updated

--- src/components/Buttons/PillButton.js
+++ src/components/Buttons/PillButton.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './style.css'
 const PillButton = ( { text, backgroundColor, clickHandler }) => {
-  return (
+  return clickHandler ?  
     <button className= {`pill-button pill-button--${backgroundColor}`} onClick={()=> clickHandler && clickHandler(text)}>
       { text }
     </button>
-  )
+  : 
+  <button className= {`card__pill-button pill-button--${backgroundColor}`} >
+      { text }
+  </button>
 }
 
 export default PillButton

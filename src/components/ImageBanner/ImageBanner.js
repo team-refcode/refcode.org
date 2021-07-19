10b3938884671fc4d0f8efc91475dc3c84308352
Brenton <brenton.strine@mailchimp.com>
2021-07-19 12:35:37 -0400

    volunteer image fix

--- src/components/ImageBanner/ImageBanner.js
+++ src/components/ImageBanner/ImageBanner.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './style.css'
 const ImageBanner = () => {
   return (
-    <div class="image-banner image-banner-1 full-width-in-main-body">
+    <div class="image-banner image-banner-1 full-width-in-main-body" style={{backgroundImage: 'url(/img/nasa-hat.jpg)'}}>
       <div class="banner-text">It's not about the code, it's about the connections.</div>
     </div>
   )
